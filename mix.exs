9430d2f42f6e2a0cbcbed9e1e17681b09256efdc
Matt Sawka <matt.sawka@perceptivesoftware.com>
2015-05-22 11:31:15 -0500

    bump fleet and messaging

--- mix.exs
+++ mix.exs
@@ -39,16 +39,17 @@ defmodule OpenAperture.WorkflowOrchestrator.Mixfile do
       {:markdown, github: "devinus/markdown", only: [:test]},     
 
       {:poison, "~> 1.3.1"},
-      {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "e791d59c5bfaec9daa5cf7397401795ccd064a2a", override: true},
+      {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "8c51d099ec79473b23b3c385c072e6bf2219fba7", override: true},
       {:openaperture_manager_api, git: "https://github.com/OpenAperture/manager_api.git", ref: "5d442cfbdd45e71c1101334e185d02baec3ef945", override: true},
       {:openaperture_overseer_api, git: "https://github.com/OpenAperture/overseer_api.git", ref: "4d65d2295f2730bc74ec695c32fa0d2478158182", override: true},
 
-      {:openaperture_fleet, git: "https://github.com/OpenAperture/fleet.git", ref: "6f487b355d4dd02d8455f2f86dfc23334a446fc9", override: true},
+      {:openaperture_fleet, git: "https://github.com/OpenAperture/fleet.git", ref: "2e63b7889c76f4d3b749146f3ebceb01702cf012", override: true},
       {:openaperture_workflow_orchestrator_api, git: "https://github.com/OpenAperture/workflow_orchestrator_api.git", ref: "b5b027d860c367d34ec116292fd8e7e4ca07623f", override: true},
       
       {:timex_extensions, git: "https://github.com/OpenAperture/timex_extensions.git", ref: "ab9d8820625171afbb80ccba1aa48feeb43dd790", override: true},
 
       {:timex, "~> 0.12.9"},
+      {:fleet_api, "~> 0.0.5"},
 
       #test dependencies
       {:exvcr, github: "parroty/exvcr", override: true},
